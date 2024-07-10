ff12a9d9e42a74344756b95d58a9d6d1ed124ecd
sue06004 <sue06004@naver.com>
2024-07-10 20:42:51 +0900

    feat: Member Entity ì생성

---
+++ Week2/kwh/Oauth/src/main/java/org/example/oauth/member/Member.java
@@ -0,0 +1,38 @@
+package org.example.oauth.member;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, length = 100)
+    private String email;
+    @Column(length = 100)
+    private String password;
+
+    @Builder.Default
+    private String role = "ROLE_USER";
+    @Builder.Default
+    private Boolean enabled = false;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
