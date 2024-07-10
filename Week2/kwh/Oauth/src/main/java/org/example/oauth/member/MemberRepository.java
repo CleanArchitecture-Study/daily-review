efb96c79910a53f6861b6bd2da9ea3f94920c86f
sue06004 <sue06004@naver.com>
2024-07-10 21:07:43 +0900

    feat: Mem: 사용자 이름으로 ãMember를 ã찾는 메서ë ì¶ 추가

--- Week2/kwh/Oauth/src/main/java/org/example/oauth/member/MemberRepository.java
+++ Week2/kwh/Oauth/src/main/java/org/example/oauth/member/MemberRepository.java
@@ -3,5 +3,8 @@ package org.example.oauth.member;
 import org.example.oauth.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
 }
