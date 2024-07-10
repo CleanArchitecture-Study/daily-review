5bc789ca517a8deb2e14d1c93d845edd8192cdca
sue06004 <sue06004@naver.com>
2024-07-10 21:02:14 +0900

    feat: CustomUserDetails 생성

---
+++ Week2/kwh/Oauth/src/main/java/org/example/oauth/member/model/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package org.example.oauth.member.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Member member;
+
+
+    public Long getId(){
+        return member.getId();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority(){
+            @Override
+            public String getAuthority() {
+                return member.getRole();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return member.getEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+}
